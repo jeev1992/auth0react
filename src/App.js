0dc507bd0a2d43e713fc8e3926eeebabebc7900f
Jeevendra Singh <jeevendra.singh1992@gmail.com>
2022-09-26 23:21:20 +0530

    auth0-react-app

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import { useAuth0 } from '@auth0/auth0-react'
+import React from 'react';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import User from './components/User';
+
+function App() {
+  const { isAuthenticated } = useAuth0()
+
+  return (
+    <div className="App">
+      {!isAuthenticated ? (
+        <div>
+          <p>Login</p>
+          <Login />
+        </div>
+      ) : (
+        <div>
+          <Logout />
+          <User />
+        </div>)}
+
+    </div>
+  );
+}
+
+export default App;
